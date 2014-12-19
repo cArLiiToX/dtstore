2f1ee014b6b4dce523e8691fc3e63280f8c19270
cArLiiToX <carlos@xng.bz>
2014-12-18 23:24:14 -0500

    fixes

--- AWS.php
+++ AWS.php
@@ -22,7 +22,7 @@ require 'AWS/lib/AmazonECS.class.php';
 
 try
 {
-    $amazonEcs = new AmazonECS(AWS_API_KEY, AWS_API_SECRET_KEY, 'US', AWS_ASSOCIATE_TAG);
+    $amazonEcs = new AmazonECS(AWS_API_KEY, AWS_API_SECRET_KEY, 'COM', AWS_ASSOCIATE_TAG);
 
     // for the new version of the wsdl its required to provide a associate Tag
     // @see https://affiliate-program.amazon.com/gp/advertising/api/detail/api-changes.html?ie=UTF8&pf_rd_t=501&ref_=amb_link_83957571_2&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=&pf_rd_s=assoc-center-1&pf_rd_r=&pf_rd_i=assoc-api-detail-2-v2
