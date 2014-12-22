0b522af9383e81ec83c07aa432a55357e2a4a1b0
cArLiiToX <carlos@xng.bz>
2014-12-21 19:32:45 -0500

    agregados cambio

--- AWS.php
+++ AWS.php
@@ -1,4 +1,5 @@
 <?php
+
 error_reporting(E_ALL);
 
 require_once('app/Mage.php');
@@ -22,17 +23,15 @@ $result = $write->query($sql);
 echo '<pre>';
 while ($row = $result->fetch()) {
     //var_dump($row);
-
     //var_dump($row['link']);
 
     $code = explode('/dp/', $row['link']);
 
     //var_dump($code);
-
     //var_dump('http://www.amazon.com/gp/aw/d/' . $code[1]);
 
-       
-   
+
+
     var_dump('http://www.amazon.com/gp/aw/d/' . $code[1]);
     $html = file_get_contents('http://www.amazon.com/gp/aw/d/' . $code[1]);
     var_dump($html);
@@ -49,23 +48,46 @@ while ($row = $result->fetch()) {
 
     var_dump($Items);
 
+    if (count($Items[1])>0) {
+        var_dump('http://www.amazon.com/gp/aw/d/' . $code[1]);
+        $html = file_get_contents('http://www.amazon.com/gp/aw/d/' . $code[1]);
+        var_dump($html);
+// Find all article blocks
+
+        $patron = '/<b>Price:(.*)&nbsp;</';
+        preg_match_all($patron, $html, $coincidencias);
+        var_dump($coincidencias);
+
+
+        foreach ($coincidencias as $key => $value) {
+            $Items[] = str_replace('</b>&nbsp;$', '', $value);
+        }
+
+        var_dump($Items);
+    }
+
     // get product model by product id, assuming you have known product id, $productId
     $_product = Mage::getModel('catalog/product')->load($row['product_id']);
+
+
+
     $NuevoPrecio = $Items[1];
-    
+
+
+
     $PrecioImportacion = $_product->getResource()->getAttribute('precio_importacion')->getFrontend()->getValue($_product);
 
-    
-    var_dump((int)round($NuevoPrecio[0]));
-    var_dump((int)round($PrecioImportacion));
-    var_dump((int)round($TRM));
-    
-    $NuevoPrecioCalculado = (int)round($NuevoPrecio[0])+(int)round($PrecioImportacion);
+
+    var_dump((int) round($NuevoPrecio[0]));
+    var_dump((int) round($PrecioImportacion));
+    var_dump((int) round($TRM));
+
+    $NuevoPrecioCalculado = (int) round($NuevoPrecio[0]) + (int) round($PrecioImportacion);
     var_dump($NuevoPrecioCalculado);
-    
+
     $New_Price = ($NuevoPrecioCalculado * $TRM) + 10000;
     $_product->setPrice($New_Price)->save();
-    
+
     var_dump($html);
 }
 echo '</pre>';
