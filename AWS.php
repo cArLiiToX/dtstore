805f62bf122d7d7a06e7ba648e4d1cb3d53785b5
cArLiiToX <carlos@xng.bz>
2014-12-21 19:36:45 -0500

    agregados cambio

--- AWS.php
+++ AWS.php
@@ -47,7 +47,7 @@ while ($row = $result->fetch()) {
     }
 
     var_dump($Items);
-    var_dump(count($Items[1]));
+    var_dump('---->'.count($Items[1]).'<------');
     if (count($Items[1])>0) {
         var_dump('http://www.amazon.com/gp/aw/d/' . $code[1]);
         $html = file_get_contents('http://www.amazon.com/gp/aw/d/' . $code[1]);
