d1a50e2fa0ee863db72e2cfa9c6e8e68155f4f0a
cArLiiToX <carlos@xng.bz>
2015-01-01 18:13:02 -0500

    habilitado el contador

--- AWS.php
+++ AWS.php
@@ -11,8 +11,6 @@ Mage::register('isSecureArea', true);
 
 var_dump('http://dolar.wilkinsonpc.com.co/widgets/gratis/dolar-cop-usd-1.html');
 $html = file_get_contents('http://dolar.wilkinsonpc.com.co/widgets/gratis/dolar-cop-usd-1.html');
-//var_dump($html);
-// Find all article blocks
 
 $patron = '/<div id="widget_valor">(.*)</';
 preg_match_all($patron, $html, $coincidencias);
@@ -40,21 +38,18 @@ if ($NewValueRoundUp50 < 50) {
 var_dump('Nuevo precio dolar: ' . $NewValueRoundUp50);
 //die($NewValueRoundUp50);
 
-
-$sql = 'SELECT * FROM  amazon_products_amazon_product as app, amazon_products_amazon as aps WHERE aps.entity_id = app.amazon_id and  status = 1';
-// fetch write database connection that is used in Mage_Core module
-$write = Mage::getSingleton('core/resource')->getConnection('core_write');
-
-// now $write is an instance of Zend_Db_Adapter_Abstract
-$result = $write->query($sql);
-
+$productsCollection = Mage::getResourceModel('catalog/product_collection')
+        ->addAttributeToFilter('amazon_link', array('notnull' => true))
+        ->setCurPage(1)->setPageSize(100)
+        ->load();
 
 echo '<pre>';
-while ($row = $result->fetch()) {
-    //var_dump($row);
+//while ($row = $result->fetch()) {
+foreach ($productsCollection as $product) {
+    //   //var_dump($row);
     //var_dump($row['link']);
-
-    $code = explode('/dp/', $row['link']);
+    $row = $product->getData();
+    $code = explode('/dp/', $row['amazon_link']);
 
     //var_dump($code);
     //var_dump('http://www.amazon.com/gp/aw/d/' . $code[1]);
@@ -783,7 +778,7 @@ while ($row = $result->fetch()) {
     }
 
     // get product model by product id, assuming you have known product id, $productId
-    $_product = Mage::getModel('catalog/product')->load($row['product_id']);
+    $_product = Mage::getModel('catalog/product')->load($row['entity_id']);
 
 
 
