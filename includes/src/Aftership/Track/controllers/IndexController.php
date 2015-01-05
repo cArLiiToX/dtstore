b9fdd05294c44460619861081cffe12ff0e13444
cArLiiToX <carlos@xng.bz>
2015-01-04 22:03:53 -0500

    borrado archivos que no deben ir en el versionado

--- includes/src/Aftership/Track/controllers/IndexController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Aftership_Track_IndexController extends Mage_Core_Controller_Front_Action {        
-
-    public function indexAction() {
-    }
-}
-?>
--- includes/src/Aftership_Track_Helper_Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Aftership_Track_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
-?>
