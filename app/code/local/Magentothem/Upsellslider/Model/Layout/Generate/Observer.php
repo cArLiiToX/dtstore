76da6b7bfea4f1b03be8e3dbce18b56ce42c1c4a
cArLiiToX <carlos@xng.bz>
2015-01-16 01:21:36 -0500

    correccion login

--- app/code/local/Magentothem/Upsellslider/Model/Layout/Generate/Observer.php
+++ app/code/local/Magentothem/Upsellslider/Model/Layout/Generate/Observer.php
@@ -7,4 +7,7 @@ class Magentothem_Upsellslider_Model_Layout_Generate_Observer {
 		$enabled = Mage::getStoreConfig('upsellslider/upsellslider_config/enabled');
 		return Mage::getSingleton('core/layout')->getBlock('magentothem_upsellslider_head');
     }
+    public function addJqueryLibrary() {
+        return true;
+    }
 }
